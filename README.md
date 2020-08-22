6648df9af82b154ccc34cf1c58c5f17d018fde45
helano-pessoa <hp@alu.ufc.br>
2020-08-22 01:23:56 -0300

    delete some files

--- README.md
+++
@@ -1,3 +0,0 @@
-# data-science-2020.1
-
-Link to Drawio: https://app.diagrams.net/#Hhelano-pessoa%2Fdata-science-2020.1%2Fmaster%2FUntitled%20Diagram.drawio
--- run.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm -it \
---network='host' \
--v `pwd`/notebooks:/data-science/notebooks \
-datascience:latest \
-jupyter notebook --allow-root
