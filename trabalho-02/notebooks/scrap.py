273564ddb253d13d5e03f2fc1530d0020dccdb2a
helano-pessoa <hp@alu.ufc.br>
2020-08-23 10:34:13 -0300

    spotify perceptron

--- trabalho-02/notebooks/scrap.py
+++ trabalho-02/notebooks/scrap.py
@@ -1,7 +1,7 @@
 import spotipy.util as util
 
-username = 'vincyhu' #input your username
-client_id ='6dc054e3d1194241874db8c8e7fd446f' # input your client_id
+username = 'username' #input your username
+client_id ='client_id' # input your client_id
 client_secret = '' # input your client_secret
 redirect_uri = 'http://localhost:7777/callback' # the redirect_uri we set previously
 scope = 'user-read-recently-played' # different scopes relates to different authorization 
